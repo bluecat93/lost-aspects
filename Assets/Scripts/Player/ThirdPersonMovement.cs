c812063597ef17cc8f923fb3a855234f85a9e8aa
bluecat93 <edentadmor@gmail.com>
2022-03-15 23:31:31 +0200

    Fixed isgrounded with raycast

--- Assets/Scripts/Player/ThirdPersonMovement.cs
+++ Assets/Scripts/Player/ThirdPersonMovement.cs
@@ -88,24 +88,24 @@ public class ThirdPersonMovement : MonoBehaviour
 
             isMoving = direction.magnitude >= 0.1f;
 
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+
+            Vector3 directionMod = (isClimbable || !CheckIsGrounded() ? Vector3.forward : Vector3.back);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * directionMod;
+
+
             if (isMoving)
             {
                 isSprinting = Input.GetKey(SprintKey);
 
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
                 float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-                // Debug.Log("Angle: " + angle + " targetAngle: " + targetAngle + " turnSmoothVelocity: " + turnSmoothTime + " turnSmoothTime: " + turnSmoothTime + " eulerAngles: " + transform.eulerAngles.y);
-
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-                // TODO: Since isGrounded is not reliable, need to research raycasts and understand how to use it instead to fix throwback bug
-                Vector3 directionMod = (isClimbable || isJumping ? Vector3.forward : Vector3.back);
-
-                // Vector3 directionMod = (isClimbable || isJumping || (!controller.isGrounded && !isJumping) ? Vector3.forward : Vector3.back);
-                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * directionMod;
-
                 controller.Move(moveDir.normalized * (isClimbable ? movementSpeed : 0.5f) * Time.deltaTime * (isSprinting ? SprintSpeed : 1));
 
+            }
+            else if (!isClimbable)
+            {
+                controller.Move(moveDir * -1 * 0.5f * Time.deltaTime);
             }
 
             // This must be last and as close as possible to other movements so it will allways go down.
@@ -118,16 +118,13 @@ public class ThirdPersonMovement : MonoBehaviour
                 transform.eulerAngles = new Vector3(transform.eulerAngles.x, mainCamera.transform.eulerAngles.y, transform.eulerAngles.z);
             }
         }
-
     }
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        var currClimbAngle = Mathf.Round(Vector3.Angle(hit.normal, Vector3.up));
-
-        isClimbable = currClimbAngle <= maxClimbAngle;// || hit.moveDirection.y <= 0;
-        // Debug.Log("isClimbable " + isClimbable + ", currClimbAngle " + currClimbAngle + ", maxClimbAngle " + maxClimbAngle + ", hit.moveDirection.y" + hit.moveDirection.y);
+        isClimbable = Mathf.Round(Vector3.Angle(hit.normal, Vector3.up)) <= maxClimbAngle;
     }
+
     private void HandleCrouchInput()
     {
         if (Input.GetKeyDown(CrouchKey) && !isCrouching)
@@ -150,7 +147,11 @@ public class ThirdPersonMovement : MonoBehaviour
         if (Input.GetKey(DodgeKey))
         {
             playerAnim.SetTrigger("Roll");
-
         }
+    }
+
+    private bool CheckIsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 0.1f);
     }
 }
