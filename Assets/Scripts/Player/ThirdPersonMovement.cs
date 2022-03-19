113d90ad4e790e8f82e07d07123360c45605f6b1
bluecat93 <edentadmor@gmail.com>
2022-03-19 11:54:02 +0200

    moved dodge param to be variables

--- Assets/Scripts/Player/ThirdPersonMovement.cs
+++ Assets/Scripts/Player/ThirdPersonMovement.cs
@@ -31,6 +31,11 @@ public class ThirdPersonMovement : MonoBehaviour
     [SerializeField] private float movementSpeed = 5.0f;
     [SerializeField] private float turnSmoothTime = 0.1f;
 
+    // Dodging Variables
+    [SerializeField] private float dashSpeed = 20f;
+    [SerializeField] private float dashTime = 0.25f;
+
+
     #endregion
 
     #region properties
@@ -141,11 +146,6 @@ public class ThirdPersonMovement : MonoBehaviour
             return this.CpslCollider.height;
         }
     }
-
-    // Dodging Variables
-    public float DashSpeed { get; set; }
-    public float DashTime { get; set; }
-
     private float OriginalStepOffset { get; set; }
 
     #endregion
@@ -340,9 +340,9 @@ public class ThirdPersonMovement : MonoBehaviour
     {
         float startTime = Time.time;
 
-        while (Time.time < startTime + this.DashTime)
+        while (Time.time < startTime + this.dashTime)
         {
-            this.controller.Move(this.MoveDirection * this.DashSpeed * Time.deltaTime);
+            this.controller.Move(this.MoveDirection * this.dashSpeed * Time.deltaTime);
             yield return null;
         }
 
