00c8c86f789ddb51322899bd203512fc0b1adacb
Matan Zvulun <48678786+YetItMoves-MZ@users.noreply.github.com>
2022-04-02 18:55:47 +0300

    small fix to normal movement.

--- Assets/Scripts/Player/ThirdPersonMovement.cs
+++ Assets/Scripts/Player/ThirdPersonMovement.cs
@@ -25,7 +25,12 @@ public class ThirdPersonMovement : MonoBehaviour
     [SerializeField] private float heightChange = 0.5f;
 
     [SerializeField] private float movementSpeed = 5.0f;
+    [Tooltip("Acceleration and deceleration")]
+    [SerializeField] private float speedChangeRate = 10.0f;
     [SerializeField] private float turnSmoothTime = 0.1f;
+    [Tooltip("How fast the character turns to face movement direction")]
+    [Range(0.0f, 0.3f)]
+    [SerializeField] private float rotationSmoothTime = 0.12f;
 
     // Dodging Variables
     [SerializeField] private float dodgeSpeed = 10f;
@@ -152,6 +157,10 @@ public class ThirdPersonMovement : MonoBehaviour
         }
     }
     private float OriginalStepOffset { get; set; }
+    private float AnimationBlend { get; set; }
+    private float TargetRotation { get; set; }
+    // TODO: eden helpo, it is called by ref so i cant use indexer or something...
+    private float RotationVelocity;
 
     #endregion
 
@@ -178,16 +187,94 @@ public class ThirdPersonMovement : MonoBehaviour
     {
         if (!PauseMenu.isGamePaused && this.PlyrStats.isAlive)
         {
-            this.HandleCrouchInput();
-            this.HandleDodgeInput();
-            this.HandleStopWhenDead();
-            this.HandleJump();
+            //this.HandleCrouchInput();
+            //this.HandleDodgeInput();
+            //this.HandleStopWhenDead();
+            //this.HandleJump();
             this.HandleMovement();
-            this.HandleCameraUnlock();
+            //this.HandleCameraUnlock();
         }
     }
 
     private void HandleMovement()
+    {
+        // set target speed based on move speed, sprint speed and if sprint is pressed
+        float targetSpeed = IsDashing ? dashModifier : movementSpeed;
+
+        // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
+
+        // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+        // if there is no input, set the target speed to 0
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector2 twoDimentionMovement = new Vector2(horizontal, vertical);
+        bool isMoving = twoDimentionMovement == Vector2.zero;
+        if (isMoving) targetSpeed = 0.0f;
+
+        // a reference to the players current horizontal velocity
+        float currentHorizontalSpeed = new Vector3(controller.velocity.x, 0.0f, controller.velocity.z).magnitude;
+
+        float speedOffset = 0.1f;
+        float speed;
+
+        //not useing analog at the moment so i commented that for now. TODO: insert analog movement later... maybe?
+        //float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
+        float inputMagnitude = 1f;
+
+        // accelerate or decelerate to target speed
+        if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
+        {
+            // creates curved result rather than a linear one giving a more organic speed change
+            // note T in Lerp is clamped, so we don't need to clamp our speed
+            speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * speedChangeRate);
+
+            // round speed to 3 decimal places
+            speed = Mathf.Round(speed * 1000f) / 1000f;
+        }
+        else
+        {
+            speed = targetSpeed;
+        }
+        AnimationBlend = Mathf.Lerp(AnimationBlend, targetSpeed, Time.deltaTime * speedChangeRate);
+
+        // normalise input direction
+        Vector3 inputDirection = new Vector3(horizontal, 0.0f, vertical).normalized;
+
+        // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+        // if there is a move input rotate player when the player is moving
+        if (!isMoving)
+        {
+            TargetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, TargetRotation, ref RotationVelocity, rotationSmoothTime);
+
+            // rotate to face input direction relative to camera position
+            // transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+
+        }
+
+        // rotate to face camera direction
+        if (Input.GetAxisRaw("Camera Unlocked") == 0)
+        {
+            transform.rotation = new Quaternion(0.0f, playerCamera.rotation.y, 0.0f, playerCamera.rotation.w);
+        }
+
+        Vector3 targetDirection = Quaternion.Euler(0.0f, TargetRotation, 0.0f) * Vector3.forward;
+
+        // move the player
+        //_controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+        // delete this and uncomment the line above when working with jumping nstuff...
+        controller.Move(targetDirection.normalized * (speed * Time.deltaTime) + new Vector3(0.0f, 0.0f, 0.0f) * Time.deltaTime);
+
+        // update animator if using character
+        // if (_hasAnimator)
+        // {
+        //     _animator.SetFloat(_animIDSpeed, _animationBlend);
+        //     _animator.SetFloat(_animIDMotionSpeed, inputMagnitude);
+        // }
+    }
+
+    private void DeprecatedHandleMovement()
     {
         // Gets vertical and horizontal axises
         float horizontal = Input.GetAxisRaw("Horizontal");
