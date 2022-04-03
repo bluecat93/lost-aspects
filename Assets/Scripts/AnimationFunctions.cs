7bb346c2d54a1e519b94659c2836ab1e8bedff25
Matan Zvulun <48678786+YetItMoves-MZ@users.noreply.github.com>
2022-04-03 12:07:55 +0300

    added useful animations function and removed unity third person movement script

---
+++ Assets/Scripts/AnimationFunctions.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Animation
+{
+    public class Functions : MonoBehaviour
+    {
+        public bool AnimatorIsPlaying(Animator animator)
+        {
+            return animator.GetCurrentAnimatorStateInfo(0).length >
+            animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+        }
+
+        public bool AnimatorIsPlaying(Animator animator, string stateName)
+        {
+            return AnimatorIsPlaying(animator) && animator.GetCurrentAnimatorStateInfo(0).IsName(stateName);
+        }
+    }
+}
